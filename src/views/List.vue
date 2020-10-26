2789f64fb21cdb857e97209587e6d2394e1b8f05
harmyderoman <romangarmider@gmail.com>
2020-10-27 00:42:04 +0200

    initial

---
+++ src/views/List.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>List</h1>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue"
+
+  export default defineComponent({
+    name: "List",
+    components: {}
+  })
+</script>
