7f15f2cfe8d8d01e7dd988f987d6d13fab0a2299
harmyderoman <romangarmider@gmail.com>
2020-11-18 12:37:15 +0200

    Neo router links

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <template>
   <div id="app">
+    <div class="nav-wrapper position-relative">
+      <ul class="nav nav-pills nav-fill flex-column flex-md-row">
+        <li class="nav-item">
+          <router-link
+            class="nav-link mb-sm-3 mb-md-0"
+            data-toggle="tab"
+            to="/"
+            active-class="active"
+            exact
+            >List Of Notes</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link
+            class="nav-link mb-sm-3 mb-md-0"
+            data-toggle="tab"
+            to="/note"
+            active-class="active"
+            exact
+            >Create Note</router-link
+          >
+        </li>
+      </ul>
+    </div>
     <nav>
       <router-link class="router-link" to="/" exact>List Of Notes</router-link>
       <router-link class="router-link" to="/note" exact
@@ -16,3 +40,7 @@
     name: "App"
   }
 </script>
+
+<style>
+  @import "./assets/css/neumorphism.css";
+</style>
