6a3e02c0c5b348ca5d69ce3b8c14b2b07d093195
harmyderoman <romangarmider@gmail.com>
2021-05-15 09:29:26 +0300

    Deleting note on main page added

--- src/views/List.vue
+++ src/views/List.vue
@@ -17,28 +17,89 @@
           {{ todo.text }}
         </li>
       </ul>
+    </div>
+    <div class="card-footer">
       <button
         class="btn btn-sm btn-primary mr-3"
         @click="$router.push(`/note/${note.id}`)"
       >
         Go to note
       </button>
+      <button
+        class="btn btn-sm btn-primary text-danger mr-3"
+        type="button"
+        @click="onDeleteNote(note.id)"
+      >
+        Delete Note
+      </button>
     </div>
   </div>
+  <modal-window :show="showModal" @onClose="closeModal">
+    <template v-slot:modal-header>
+      Are you shure that you want to delete this note?
+    </template>
+    <template v-slot:modal-body>
+      This note will be delete forever!
+    </template>
+    <template v-slot:modal-actions
+      ><button
+        type="button"
+        class="btn btn-primary text-success"
+        @click="confirmDelete(true)"
+      >
+        Confirm
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary text-danger ml-auto"
+        @click="confirmDelete(false)"
+      >
+        Cancel
+      </button></template
+    >
+  </modal-window>
   <p v-if="!notes.length">
     There is no notes right now!<br />
     Click Create Note button to create one.
   </p>
 </template>
 
-<script>
-  export default {
-    computed: {
-      notes() {
-        return this.$store.state.notes
+<script lang="ts">
+  import ModalWindow from "@/components/ModalWindow.vue"
+  import store from "@/store"
+  import { defineComponent, computed, ref } from "vue"
+
+  export default defineComponent({
+    components: { ModalWindow },
+    setup() {
+      const notes = computed(() => store.state.notes)
+
+      const showModal = ref(false)
+
+      const onDeleteNote = (noteId: number) => {
+        showModal.value = true
+        store.commit("setCurrentId", noteId)
+      }
+      const confirmDelete = (permission: boolean): void => {
+        if (permission) {
+          store.commit("deleteNote")
+        }
+        closeModal()
+      }
+      function closeModal() {
+        store.commit("setCurrentId", 0)
+        showModal.value = false
+      }
+
+      return {
+        notes,
+        onDeleteNote,
+        showModal,
+        closeModal,
+        confirmDelete
       }
     }
-  }
+  })
 </script>
 
 <style>
