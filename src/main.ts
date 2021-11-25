913622847a4c35782e57a08b9d22e30e2a37835e
harmyderoman <romangarmider@gmail.com>
2021-11-25 12:01:26 +0200

    use pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import "./assets/css/neumorphism.css";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(createPinia()).use(router).mount('#app')
