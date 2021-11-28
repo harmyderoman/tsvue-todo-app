eeaae3fa5d087d2785df5b8f96bdda9b46d1d490
harmyderoman <romangarmider@gmail.com>
2021-11-28 19:08:23 +0200

    test fix

--- tests/unit/NoteTitle.spec.js
+++ tests/unit/NoteTitle.spec.js
@@ -1,66 +1,66 @@
-import { shallowMount, mount } from "@vue/test-utils"
-import NoteTitle from "@/components/NoteTitle"
+import { shallowMount, mount } from '@vue/test-utils'
+import NoteTitle from '@/components/NoteTitle'
 
-describe("NoteTitle", () => {
-  it("it shows 'Click to edit Title' when it's no title", () => {
-    const title = ""
-    const wrapper = shallowMount(NoteTitle, {
-      props: {
-        title
-      }
-    })
-    expect(wrapper.text()).toContain("Click to edit Title")
-  })
-  it("it shows title that it get from the props", () => {
-    const title = "Test"
-    const wrapper = shallowMount(NoteTitle, {
-      props: {
-        title
-      }
-    })
-    expect(wrapper.text()).toContain("Test")
-  })
-  it("contain h3 and don't contain input, when mounted", () => {
-    const title = ""
-    const wrapper = mount(NoteTitle, {
-      props: {
-        title
-      }
-    })
-    const h3 = wrapper.find("h3")
-    expect(h3.isVisible()).toBe(true)
-  })
-  it("contain input when clicked", async () => {
-    const title = ""
-    const wrapper = mount(NoteTitle, {
-      props: {
-        title
-      }
-    })
-    const h3 = wrapper.find("h3")
+describe('NoteTitle', () => {
+	it("it shows 'Click here to write a title' when it's no title", () => {
+		const title = ''
+		const wrapper = shallowMount(NoteTitle, {
+			props: {
+				title,
+			},
+		})
+		expect(wrapper.text()).toContain('Click here to write a title')
+	})
+	it('it shows title that it get from the props', () => {
+		const title = 'Test'
+		const wrapper = shallowMount(NoteTitle, {
+			props: {
+				title,
+			},
+		})
+		expect(wrapper.text()).toContain('Test')
+	})
+	it("contain h3 and don't contain input, when mounted", () => {
+		const title = ''
+		const wrapper = mount(NoteTitle, {
+			props: {
+				title,
+			},
+		})
+		const h3 = wrapper.find('h3')
+		expect(h3.isVisible()).toBe(true)
+	})
+	it('contain input when clicked', async () => {
+		const title = ''
+		const wrapper = mount(NoteTitle, {
+			props: {
+				title,
+			},
+		})
+		const h3 = wrapper.find('h3')
 
-    await h3.trigger("click")
+		await h3.trigger('click')
 
-    expect(wrapper.find("input").isVisible()).toBe(true)
-  })
-  it("emits event on text input and after enter keydown triggered it hide input and show title", async () => {
-    const text = "Test"
-    const title = ""
-    const wrapper = mount(NoteTitle, {
-      props: {
-        title
-      }
-    })
-    const h3 = wrapper.find("h3")
+		expect(wrapper.find('input').isVisible()).toBe(true)
+	})
+	it('emits event on text input and after enter keydown triggered it hide input and show title', async () => {
+		const text = 'Test'
+		const title = ''
+		const wrapper = mount(NoteTitle, {
+			props: {
+				title,
+			},
+		})
+		const h3 = wrapper.find('h3')
 
-    await h3.trigger("click")
-    const input = wrapper.find('input[type="text"]')
-    expect(input.isVisible()).toBe(true)
-    input.setValue(text)
-    expect(wrapper.emitted("update-title")).toBeTruthy()
-    expect(wrapper.emitted("update-title")[0]).toEqual([text])
+		await h3.trigger('click')
+		const input = wrapper.find('input[type="text"]')
+		expect(input.isVisible()).toBe(true)
+		input.setValue(text)
+		expect(wrapper.emitted('update-title')).toBeTruthy()
+		expect(wrapper.emitted('update-title')[0]).toEqual([text])
 
-    await input.trigger("keydown.enter")
-    expect(h3.isVisible()).toBe(true)
-  })
+		await input.trigger('keydown.enter')
+		expect(h3.isVisible()).toBe(true)
+	})
 })
